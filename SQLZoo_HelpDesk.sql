63c09a85c0e0110c48bd2b24538c3b3b4a45bde1
lyr-meg <lyr-meg@hotmail.com>
2024-09-24 23:09:55 -0400

    added medium difficulty level questions

--- SQLZoo_HelpDesk.sql
+++ SQLZoo_HelpDesk.sql
@@ -31,7 +31,7 @@ Staff on Issue.Assigned_to = Staff.Staff_code
 left join Level on Staff.Level_code = Level.Level_code
 where Level.Manager = "Y"
 
--- Q5s
+-- Q5
 select
 Shift_date,
 Shift_type,
@@ -40,3 +40,37 @@ Last_name
 from Shift left join Staff
 on Shift.Manager = Staff.Staff_code
 order by Shift_date asc, Shift_type asc
+
+-- Q6
+select
+Customer.Company_name,
+count(distinct Call_ref) as cc
+from Issue
+left join Caller on Issue.Caller_id = Caller.Caller_id
+left join Customer on Caller.Company_ref = Customer.Company_ref
+group by Customer.Company_name
+having cc>18
+
+-- Q7
+select First_name,
+Last_name from Caller
+where Caller_id not in (select distinct Caller_id from Issue)
+
+
+-- Q8
+with company_calls as (select
+Customer.Company_name,
+Customer.Contact_id,
+count(distinct Call_ref) as nc
+from Customer
+inner join Caller on Customer.Company_ref = Caller.Company_ref
+left join Issue on Issue.Caller_id = Caller.Caller_id
+group by Customer.Company_ref
+having nc<5)
+select
+Company_name,
+Caller.First_name,
+Caller.Last_name,
+nc
+from company_calls left join Caller
+on company_calls.Contact_id = Caller.Caller_id
